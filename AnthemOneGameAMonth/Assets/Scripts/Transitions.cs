06c6867b480abe6936419e7c28d8dbcf88ea131f
Andrew Seba <sebaturtle@gmail.com>
2017-03-16 13:18:05 -0500

    Starting some transitional Effects

---
+++ AnthemOneGameAMonth/Assets/Scripts/Transitions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Andrew Seba
+/// Description: Helps with transitions.
+/// </summary>
+public class Transitions : MonoBehaviour {
+
+    public GameObject panelToFade;
+    public float lengthOfFade = 2f;
+
+    /// <summary>
+    /// Stats the coroutine to fade out.
+    /// </summary>
+	public void _FadeToFullAlpha()
+    {
+
+    }
+    
+}
